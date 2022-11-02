911c74e26f0aa7a5dc9d24752e48704e767ae5cb
bappy-ahashan <bappyahashan@gmail.com>
2022-11-03 04:15:18 +0600

    add new

--- src/Cosmatic/Cosmatic.js
+++ src/Cosmatic/Cosmatic.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import './cosmatic.css'
 
 const Cosmatic = (props) => {
     const { name, price, _id } = props.cosmatic;
+    const addToCart = (name) => {
+        console.log('item added', name);
+
+    }
     return (
-        <div>
+        <div className='product'>
             <h1>Buy This: {name}</h1>
             <p>Only For: {price}</p>
             <p>It has Id : {_id}</p>
+            <button onClick={() => addToCart(name)} >Buy Now</button>
 
         </div>
     )
