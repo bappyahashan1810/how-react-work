a20f89a68cdbfc3b6f010526ec20f62d36131b64
bappy-ahashan <bappyahashan@gmail.com>
2022-11-03 14:08:11 +0600

    add product localstore with object

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,14 +1,21 @@
 const addToDb = (name) => {
-    const quantity = localStorage.getItem(name);
+
+    let shoppingCart = {};
+
+    const quantity = shoppingCart[name];
     if (quantity) {
-        console.log("Already Exits", name);
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(name, newQuantity);
+
+        const newQuantity = quantity + 1;
+        shoppingCart[name] = newQuantity;
+        // localStorage.setItem(name, newQuantity);
     }
     else {
-        console.log('New Iteams', name);
-        localStorage.setItem(name, 1);
+        shoppingCart[name] = 1;
+
+        // localStorage.setItem(name, 1);
     }
+
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 
 }
 export { addToDb };
