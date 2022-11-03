3680acff613408e339bd4d8c4c827860b41d862a
bappy-ahashan <bappyahashan@gmail.com>
2022-11-03 11:56:54 +0600

    start today

--- src/Cosmatic/Cosmatic.js
+++ src/Cosmatic/Cosmatic.js
@@ -4,7 +4,7 @@ import './cosmatic.css'
 const Cosmatic = (props) => {
     const { name, price, _id } = props.cosmatic;
     const addToCart = (name) => {
-        console.log('item added', name);
+        localStorage.setItem(name, 1);
 
     }
     return (
@@ -13,6 +13,7 @@ const Cosmatic = (props) => {
             <p>Only For: {price}</p>
             <p>It has Id : {_id}</p>
             <button onClick={() => addToCart(name)} >Buy Now</button>
+            <button onClick={() => addToCart(name)}>Purches</button>
 
         </div>
     )
